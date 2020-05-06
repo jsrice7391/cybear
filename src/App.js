ee252856037e00edce439683020da8a98bd0c732
jsrice7391 <jsrice7391@gmail.com>
2020-05-06 01:37:21 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './cybear.svg';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Software Development Videos, DevOps and Other Content coming soon!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/jsrice7391"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub 
         </a>
       </header>
     </div>
